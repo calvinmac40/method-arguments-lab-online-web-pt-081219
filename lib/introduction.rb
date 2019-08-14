8270af37a7a92be5fa79d63302adcf58ca67eddb
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-14 00:37:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,10 @@ def introduction(name)
   puts "Hey, My name is #{name}."
 end
 
-def introduction_wit_language(name,language)
+introduction("Calvin")
+
+def introduction_with_language(name,language)
   puts "hey my name is #{name} and I'm learning #{language}."
 end
+
+introduction_with_language("Calvin", "Ruby")
